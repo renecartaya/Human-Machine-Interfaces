c944070a095887c4e895c5d4a3ced67cf4d1cce6
René Cartaya <52467158+renecartaya@users.noreply.github.com>
2022-02-12 16:32:00 +0100

    Update analog_voltage_reading.ino

--- ATmega ADC/analog_voltage_reading.ino
+++ ATmega ADC/analog_voltage_reading.ino
@@ -1,14 +1,4 @@
-/*
-  ReadAnalogVoltage
 
-  Reads an analog input on pin 0, converts it to voltage, and prints the result to the Serial Monitor.
-  Graphical representation is available using Serial Plotter (Tools > Serial Plotter menu).
-  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
-
-  This example code is in the public domain.
-
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/ReadAnalogVoltage
-*/
 
 // the setup routine runs once when you press reset:
 void setup() {
